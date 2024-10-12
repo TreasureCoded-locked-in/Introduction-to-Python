d6f88d6e7610dc7f84aa02f0c7f387abf80690cb
TreasureCoded-locked-in <akpannsikak410@gmail.com>
2024-10-12 17:12:12 +0100

    Second Commit

--- Main.py
+++ Main.py
@@ -6,8 +6,15 @@ print('The sum of 10 and 20 is',10+20)
 print('The difference between 50 and 16 is',50-16)
 print('The answer of 9*8 is',9*8)
 print('The answer to 34/2 is',34/2)
+print('The answer to this equation 10+4*5 is',10+4*5)
 # I like to Code.
 # DO you like to Code?
 # You are also a developer that's why you see this.
 #So probably
 # Or else your just doing it for smth else 🤔
+# Comments can be so random at times
+#Like these😂
+# So I'm almost done with this project
+# Do you think that my teacher will give my Five Stars on this 🤔
+# We will just have to wait and see
+# Catch you on my next comment-based project cya!👋
