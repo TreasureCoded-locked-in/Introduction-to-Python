bb6ef311413707d2f2dbb413ef4691b0bf256f05
TreasureCoded-locked-in <akpannsikak410@gmail.com>
2024-10-12 16:42:56 +0100

    First Commit

---
+++ Main.py
@@ -0,0 +1,13 @@
+print('Hello,How are you')
+print('Treasure,Nigeria')
+print('purple,reading')
+print('Gaming,Fashion')
+print('The sum of 10 and 20 is',10+20)
+print('The difference between 50 and 16 is',50-16)
+print('The answer of 9*8 is',9*8)
+print('The answer to 34/2 is',34/2)
+# I like to Code.
+# DO you like to Code?
+# You are also a developer that's why you see this.
+#So probably
+# Or else your just doing it for smth else 🤔
